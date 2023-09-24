215fb73b751c42915ed7c83a158b39fdd6ebd389
Demetrios Vlassis <95277454+TheHero9@users.noreply.github.com>
2023-09-24 19:13:07 +0300

    Create project

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Canvas shadows>
+  <Suspense fallback={null}>
+    <App />
+  </Suspense>
+  </Canvas>
+)
